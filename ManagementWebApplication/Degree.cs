724827d033bd38007b92ca050e64c341dd2e1c41
Siphosethu-Dev <75676661+Siphosethu-Dev@users.noreply.github.com>
2020-12-08 15:01:15 +0200

    Section 1

---
+++ ManagementWebApplication/Degree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementWebApplication
+{
+    public class Degree:Lecturer
+    {
+        public string DegreeName { get; set; }
+        public int Duration { get; set; }
+        public string[] Courses { get; set; }
+
+        public Degree(int elements) :base(elements)
+        {
+            Courses = new string[elements];
+        }
+
+    }
+}
---
+++ ManagementWebApplication/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementWebApplication
+{
+    interface IPerson
+    {
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string FullName { get; set; }
+    }
+}
